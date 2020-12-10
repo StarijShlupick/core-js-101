6a4c57f9715e757f14351f90a107f2c3693b6636
Maksim Burakou <maxashtet@gmail.com>
2020-12-10 12:30:47 +0300

    feat: some number tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -219,8 +219,12 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const result = +value;
+  if (result === undefined || Number.isNaN(result)) {
+    return def;
+  }
+  return result;
 }
 
 module.exports = {
